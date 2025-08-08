de0d89be9df3821bc640497c09366812b81a882b
Mahibalan-004 <mahibalan480@gmail.com>
2025-08-08 20:37:03 +0530

    env

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
+// App.js
+import React, { useEffect, useState } from 'react';
 import Contact from './components/contact';
 import './App.css';
 
 const App = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    // Fetch data from backend API
+    fetch(`${process.env.REACT_APP_API_URL}/api/some-route`)
+      .then(res => res.json())
+      .then(apiData => {
+        console.log("API Response:", apiData);
+        setData(apiData);
+      })
+      .catch(err => console.error("Error fetching API:", err));
+  }, []);
+
   return (
     <div className="container">
+      <h1>Data from Backend:</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
       
-        <Contact />
-      
+      {/* Your existing Contact component */}
+      <Contact />
     </div>
   );
 };
